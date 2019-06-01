c11d6205c520d132328d2b1f717064bc7bb0f5f6
causingbrick <1520593807@qq.com>
2019-06-01 13:41:05 +0800

    Modify the API  Filed: CreatedAt

--- ch4/github/issue.go
+++ ch4/github/issue.go
@@ -1,5 +1,9 @@
 package github
 
+import (
+	"time"
+)
+
 // IssuesURL The github Issues API.
 const IssuesURL = "https://api.github.com/search/issues"
 
@@ -11,13 +15,13 @@ type IssuesSearchResult struct {
 
 // Issue store the values for issues.
 type Issue struct {
-	NUmber     int
-	HTMLURL    string `json:"html_url"`
-	Title      string
-	State      string
-	User       *User
-	CreateadAt string `json:"Created_at"`
-	Body       string //markdown formats.
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"Created_at"`
+	Body      string    //markdown formats.
 }
 
 // User store the values for User Info.
